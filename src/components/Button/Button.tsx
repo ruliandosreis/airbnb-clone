ac8b6e4e312719c9cd32640a6d3a1b4bf34cce3a
rulian.reis <rulian.dosreis@hotmail.com>
2023-06-25 14:38:21 -0300

    ✨ feat: button component & initiating login modal

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,36 @@
+import React, { ButtonHTMLAttributes, FC } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  children: React.ReactNode;
+}
+
+const buttonVariants = cva(
+  "cursor-pointer inline-flex items-center justify-center rounded-lg text-sm font-medium transition-color focus:ring-2 focus:ring-offset-2 focus:ring-rose-500 disabled:opacity-50 !font-bold",
+  {
+    variants: {
+      variant: {
+        primary: "bg-rose-600 text-white hover:bg-rose-700 p-4 text-xl",
+        ghost:
+          "cursor-pointer bg-transparent w-full py-4 text-rose-600 rounded-lg text-xl",
+      },
+    },
+  }
+);
+
+const Button: FC<ButtonProps> = ({
+  className,
+  children,
+  variant,
+  ...props
+}) => {
+  return (
+    <button className={`${buttonVariants({ variant })}`} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/Header/LoginModal/LoginModal.tsx
+++ src/components/Header/LoginModal/LoginModal.tsx
@@ -1,9 +1,22 @@
+import Button from "@/components/Button/Button";
 import React from "react";
 
 const LoginModal = () => {
   return (
-    <div className="w-full h-full flex flex-col">
+    <div className="w-full h-full flex flex-col gap-4">
       <h3 className="text-2xl font-bold">Bem vindo ao Airbnb</h3>
+      <Button variant="primary">Continuar</Button>
+      <span className="">
+        Ligaremos ou enviaremos uma mensagem para confirmar seu número. Podem
+        ser aplicadas tarifas padrão de dados e mensagens.
+        <a
+          className="font-bold underline ml-[0.5ch]"
+          href="https://www.airbnb.com.br/help/article/2855"
+        >
+          Política de privacidade
+        </a>
+      </span>
+      <Button variant="ghost">Sair</Button>
     </div>
   );
 };
