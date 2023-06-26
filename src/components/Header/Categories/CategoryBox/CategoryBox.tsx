25a523fe1b7a45e24c3cdb1371244322e33405e1
rulian.reis <rulian.dosreis@hotmail.com>
2023-06-26 14:12:21 -0300

    🐛 fix: removing shifting layout on categories hover

--- src/components/Header/Categories/CategoryBox/CategoryBox.tsx
+++ src/components/Header/Categories/CategoryBox/CategoryBox.tsx
@@ -41,10 +41,10 @@ const CategoryBox: FC<Category> = ({
 
   return (
     <div
-      className={`flex flex-col items-center justify-center gap-2 p-3 hover:border-b-2 hover:text-neutral-800 transition cursor-pointer
+      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 border-transparent hover:border-b-neutral-300 hover:text-neutral-800 transition cursor-pointer
         ${
           selected
-            ? "border-b-2 border-b-neutral-800 text-neutral-800"
+            ? "border-b-neutral-800 hover:border-b-neutral-800 text-neutral-800"
             : "transparent text-neutral-500"
         }
       `}
