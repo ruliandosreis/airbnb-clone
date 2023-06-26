1ab1a37fee741d0098859fee51bfb753e43495ad
rulian.reis <rulian.dosreis@hotmail.com>
2023-06-25 21:02:47 -0300

    ✨feature: setting up mongo db and prisma interaction

---
+++ src/pages/api/auth/[...nextAuth].ts
@@ -0,0 +1,65 @@
+import NextAuth, { AuthOptions } from "next-auth";
+
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Credenciais inválidas");
+        }
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || user?.hasedPassword) {
+          throw new Error("Credenciais inválidas");
+        }
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.hasedPassword as string
+        );
+
+        if (!passwordMatch) {
+          throw new Error("Credenciais inválidas");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
